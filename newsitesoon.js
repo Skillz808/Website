1e464b51e38eb7db94bbcbfddad4ff327d5922ca
Aaron Duncan <aaronduncan808@proton.me>
2023-09-30 01:15:21 +0100

    audio autoplay bypass (sorta)

--- newsitesoon.js
+++ newsitesoon.js
@@ -91,5 +91,21 @@ function closeDragElement(){
     document.onmouseup = null;
     document.onmousemove = null;
   }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var audio = document.getElementById('audioplayer');
+    var hasAlreadyClicked = false;
+
+    function startAudio() {
+        if (hasAlreadyClicked == false) {
+            audio.play();
+            audio.volume = 0.5;
+            document.removeEventListener('click', startAudio);
+            hasAlreadyClicked = true;
+        }
+    }
+
+    document.addEventListener('click', startAudio);
+});
 }
 
